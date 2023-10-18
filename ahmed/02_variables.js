8700cceacda9401e95e76ce03917d1475edb6c80
ahmedali107 <116943452+ahmedali107@users.noreply.github.com>
2023-10-18 12:41:36 +0000

    operations/converstions dt

--- ahmed/02_variables.js
+++ ahmed/02_variables.js
@@ -2,6 +2,7 @@ const accountId = 6361
 let accountEmail = "ahmed@google.com"
 var accountPassword = "1234"
 accountCity = "jamkhandi"
+let accountState;
 
 //accountId = 2
 
@@ -9,5 +10,10 @@ accountEmail= "hcb@.com"
 accountPassword="2545"
 accountCity="hubli"
 
+/*
+perfer not to use var
+beacouse of issue of block scope and fuctional scope
+*/
+
 console.log(accountId);
-console.table([accountId,accountPassword,accountEmail,accountCity])
+console.table([accountId,accountPassword,accountEmail,accountCity,accountState])
---
+++ ahmed/03_datatypes.js
@@ -0,0 +1,49 @@
+"use strict";//treat all js code as newer version
+
+//alert("hello") // we are using node js not browser
+
+console.log(3
+    +
+    3) 
+// code readability should be high
+console.log("ahmed")
+
+let name="ahmed"
+let age=19
+let isLoggedIn=false 
+
+//number =>2 to power 53
+//bigint
+//string =>single code and double code accepted
+//boolean =>true/false
+//null =>standalone value it is an object
+//undefined =>
+//symbol =>unique 
+
+//object
+
+console.log(typeof undefined);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
