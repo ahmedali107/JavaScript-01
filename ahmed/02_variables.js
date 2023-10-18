1329e3a79952567464e76e36b0d16aef413be0ea
ahmedali107 <116943452+ahmedali107@users.noreply.github.com>
2023-10-18 04:19:30 +0000

    datatypes

---
+++ ahmed/02_variables.js
@@ -0,0 +1,13 @@
+const accountId = 6361
+let accountEmail = "ahmed@google.com"
+var accountPassword = "1234"
+accountCity = "jamkhandi"
+
+//accountId = 2
+
+accountEmail= "hcb@.com"
+accountPassword="2545"
+accountCity="hubli"
+
+console.log(accountId);
+console.table([accountId,accountPassword,accountEmail,accountCity])
