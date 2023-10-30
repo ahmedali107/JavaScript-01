9b4e8ee1551c42d6771806080a7ce7cb8ad89394
ahmedali107 <116943452+ahmedali107@users.noreply.github.com>
2023-10-30 12:59:10 +0000

    managenment

---
+++ ahmed/memorymanagement.js
@@ -0,0 +1,42 @@
+//++++++++++++++++++++
+
+//stack ,Heap 
+ 
+//stack(in primitive datatypes stack memory is used)
+
+//heap(in non- primitive datatypes heapmemory is used)
+
+let myRealname = "ahmedjdhalayat"
+
+let anothername = myRealname
+
+anothername = "ayesha"
+
+console.log(anothername);
+console.log(anothername);
+
+
+let user = {
+    email:"ahmed@google.com",
+    upi:"user@jjs"
+}
+console.log(user);
+
+let userTwo =user
+
+userTwo.email = "klesbca@google.com"
+
+console.log(user.email);
+console.log(userTwo.email);
+
+
+
+
+
+
+
+
+
+
+
+
