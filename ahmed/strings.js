b7ff8359e71a76779c85873bd56fa96d8f89bfac
ahmedali107 <116943452+ahmedali107@users.noreply.github.com>
2023-10-31 16:03:40 +0000

    strings

---
+++ ahmed/strings.js
@@ -0,0 +1,39 @@
+//const name ="ahmed"
+//const repoCount = 10
+
+//console.log(name+repoCount+"value");
+
+//console.log(`hello welcome to kles bca hubballi${name} and repo count is ${repoCount}`.toUpperCase());
+
+
+
+const gameName = new  String("ahmedali")
+
+
+//console.log(gameName[0]);
+//console.log(gameName.__proto__);
+
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+
+//console.log(gameName.charAt(4));
+
+//console.log(gameName.indexOf('d'));
+
+
+
+const newString = gameName.substring(0,4)
+
+console.log(newString);
+
+const anotherString = gameName.slice(-8,2)
+
+console.log(anotherString);
+
+
+const newStringOne = "    ahmed        "
+
+
+console.log(newStringOne);
+
+console.log(newStringOne.trim())
